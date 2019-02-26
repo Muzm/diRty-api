b5b13269542d8efd306601433699d2a369464c80
Acidtar acidtar <muzhiu@gmail.com>
2019-02-26 19:37:11 +0800

    [ Dockerize ]

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,9 @@ router.get('/api/getSong', async (ctx, next) => {
             ctx.response.status = 422
             ctx.body = 'Vendor or Song id is empty'
         } else {
-            let data = await axios.get(`http://${neteaseApi}/song/url?id=${ctx.request.query.id}`)
+            let data = await axios.get(
+                `http://${neteaseApi}/song/url?id=${ctx.request.query.id}`
+            )
             ctx.body = data.data
         }
     } catch (err) {
@@ -33,10 +35,13 @@ router.get('/api/getSong', async (ctx, next) => {
 
 router.get('/api/userPlayList', async (ctx, next) => {
     try {
-        let data = await axios.get(`http://${neteaseApi}/user/playlist?uid=${ctx.request.query.uid}`)
-        let limit = ctx.query.limit === 'all' ? 
-                    data.data.playlist.trackCount : 
-                    ctx.query.limit
+        let data = await axios.get(
+            `http://${neteaseApi}/user/playlist?uid=${ctx.request.query.uid}`
+        )
+        let limit =
+            ctx.query.limit === 'all'
+                ? data.data.playlist.trackCount
+                : ctx.query.limit
         ctx.body = CircularJSON.stringify(data.data)
     } catch (err) {
         console.error('Getting My Play List error: ' + err)
@@ -45,8 +50,11 @@ router.get('/api/userPlayList', async (ctx, next) => {
 
 router.get('/api/albumDetail', async (ctx, next) => {
     try {
-        let data = await axios.get(`http://${neteaseApi}/album?id=${ctx.query.id}`)
-        const limit = ctx.query.limit === 'all' ? data.data.songs.length : ctx.query.limit
+        let data = await axios.get(
+            `http://${neteaseApi}/album?id=${ctx.query.id}`
+        )
+        const limit =
+            ctx.query.limit === 'all' ? data.data.songs.length : ctx.query.limit
         data.data.songs = data.data.songs.slice(
             limit * ctx.query.offset,
             limit * ctx.query.offset + limit
@@ -59,7 +67,11 @@ router.get('/api/albumDetail', async (ctx, next) => {
 
 router.get('/api/artist/album', async (ctx, next) => {
     try {
-        let data = await axios.get(`http://${neteaseApi}/artist/album?id=${ctx.query.id}&limit=${ctx.query.limit}&offset=${ctx.query.offset * ctx.query.limit}`)
+        let data = await axios.get(
+            `http://${neteaseApi}/artist/album?id=${ctx.query.id}&limit=${
+                ctx.query.limit
+            }&offset=${ctx.query.offset * ctx.query.limit}`
+        )
         ctx.body = CircularJSON.stringify(data.data)
     } catch (e) {
         console.error('Getting album detail error' + e)
@@ -68,13 +80,18 @@ router.get('/api/artist/album', async (ctx, next) => {
 
 router.get('/api/listDetail', async (ctx, next) => {
     try {
-        let data = await axios.get(`http://${neteaseApi}/playlist/detail?id=${ctx.request.query.id}&timestamp=${new Date().getTime()}`)
-        let limit = ctx.request.query.limit === 'all' ? 
-                    data.data.playlist.trackCount : 
-                    ctx.request.query.limit
+        let data = await axios.get(
+            `http://${neteaseApi}/playlist/detail?id=${
+                ctx.request.query.id
+            }&timestamp=${new Date().getTime()}`
+        )
+        let limit =
+            ctx.request.query.limit === 'all'
+                ? data.data.playlist.trackCount
+                : ctx.request.query.limit
         let offset = ctx.request.query.offset
         data.data.playlist.tracks = data.data.playlist.tracks.slice(
-            limit * offset, 
+            limit * offset,
             limit * offset + limit
         )
         ctx.body = data.data // add offset and limit
