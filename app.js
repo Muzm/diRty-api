f2bfc8df4ccaa5dc47a56b9ddfcdf8e30487fee0
Acidtar acidtar <Muzm@users.noreply.github.com>
2019-03-27 13:02:40 +0800

    Revert "refactor($core): make code clean, and add unit-test"
    This reverts commit d579642eba01b56c7f9b4c70d0f3520dc0f67fcc.

--- app.js
+++ app.js
@@ -3,22 +3,43 @@ const app = new Koa()
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
 const cors = require('@koa/cors')
-const timer = require('./middlewares/timer');
-const logger = require('./helper/logger');
-const index = require('./routes')
-const depsInstall = require('./depsInstall')
 
-depsInstall()
+const index = require('./routes/index')
+
+const install = require('./install')
+
+install() // install NeteaseCloudMusicApi
+
+// error handler
 onerror(app)
-app.use(bodyparser({ enableTypes: ['json', 'form', 'text'] }))
+
+// middlewares
+app.use(
+    bodyparser({
+        enableTypes: ['json', 'form', 'text'],
+    })
+)
 app.use(cors())
 app.use(json())
-app.use(timer(logger))
+app.use(logger())
+// app.use(require('koa-static')(__dirname + '/build')) production build from diRty
+
+// logger
+app.use(async (ctx, next) => {
+    const start = new Date()
+    await next()
+    const ms = new Date() - start
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+// routes
 app.use(index.routes(), index.allowedMethods())
 
+// error-handling
 app.on('error', (err, ctx) => {
-    logger.error('server error', err, ctx)
+    console.error('server error', err, ctx)
 })
 
 module.exports = app
---
+++ install.js
@@ -0,0 +1,62 @@
+const exec = require('child-process-promise').exec
+const fs = require('fs')
+
+const installNeteaseCloudMusicApi = async (packageTool = 'npm') => {
+  const clone = (times = 0)=> {
+    if(times > 2)
+      console.log(`Try to clone 3 times all filed
+      Please Manually clone https://github.com/Binaryify/NeteaseCloudMusicApi.git in / and run it in PORT 3002`)
+    else {
+      if(!fs.existsSync(__dirname + '/NeteaseCloudMusicApi') || times > 0) {
+        return exec(`git clone https://github.com/Binaryify/NeteaseCloudMusicApi.git`)
+                .then(function (result) {
+                    var stderr = result.stderr
+                    console.log('stderr: ', stderr)
+                    if(!result.error) {
+                      console.log(`Installed NeteaseCloudMusicApi now install its deps`)
+                      return true
+                    }
+                    console.log(`Install NeteaseCloudMusicApi failed retry`)
+                    clone(times + 1)
+                })
+                .catch(err=> console.error("clone error:", err))
+      } else console.log('NeteaseCloudMusicApi installed')
+    }
+  }
+
+  const installDeps = (times, packageTool = 'npm')=> {
+    if(times > 2) {
+      console.log(`Try to install NeteaseCloudMusicApi's deps 3 times all filed
+      Please Manually install in /NeteaseCloudMusicApi`)
+    } else {
+      if(!fs.existsSync(__dirname + '/NeteaseCloudMusicApi/node_modules') || times > 0) {
+        return exec(`${packageTool} install`, {cwd: __dirname + '/NeteaseCloudMusicApi'})
+                .then((result)=>{
+                  if(!result.error) {
+                    console.log("NeteaseCloudMusicApi's deps installed")
+                    return true
+                  } 
+                  console.log(`NeteaseCloudMusicApi's deps install failed retry`)
+                  installDeps(times + 1)
+                })
+                .catch(err => console.error('NeteaseCloudMusicApi install err:', err))
+      } else console.log("NeteaseCloudMusicApi's deps installed")
+    }
+  }
+
+  try {
+    await clone()
+    await installDeps()
+    console.log(process.platform)
+    console.log('NeteaseCloudMusicApi run in port 3002')
+    if(process.platform === 'win32') {
+      exec('set PORT=3002 & node app.js', {cwd: __dirname + '/NeteaseCloudMusicApi'})
+    } else {
+      exec('PORT=3002 node app.js', {cwd: __dirname + '/NeteaseCloudMusicApi'})
+    }
+  } catch(e) {
+    console.error('NeteaseCloudMusicApi error:', e)
+  }
+}
+
+module.exports = installNeteaseCloudMusicApi
